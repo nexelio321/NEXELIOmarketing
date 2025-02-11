fa1031ee7809716b44ce38a9171bd2410bf930f5
nexelio321 <nexeliomarketing26@gmail.com>
2025-02-11 20:53:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,3 @@ document.querySelector(".explore-btn").addEventListener("click", function (e) {
     document.querySelector("#services").scrollIntoView({ behavior: "smooth" });
 });
 
-// Video fallback check for mobile
-window.onload = function () {
-    let video = document.getElementById("hero-video");
-    if (window.innerWidth < 768) {
-        video.style.display = "none";
-    }
-};
